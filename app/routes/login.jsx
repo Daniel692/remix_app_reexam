8d1751a3b9e05ec83bd04f36a8af87d7ff1e928a
Daniel <birkusdaniel6@gmail.com>
2023-04-14 15:41:14 +0200

    logout functionality

--- app/routes/login.jsx
+++ app/routes/login.jsx
@@ -9,6 +9,7 @@ import bcrypt from "bcryptjs";
 
 export async function loader({ request }) {
     const session = await getSession(request.headers.get("Cookie"));
+    console.log(session);
     return json({ userId: session.get("userId") });
   }
 
@@ -44,37 +45,53 @@ export async function action( {request }) {
 export default function Login() {
     const dataAction = useActionData();
     const { userId } = useLoaderData();
-    return (
-        <div className="w-full max-w-xs">
-            <Form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="post" reloadDocument>
-                <h1 className="ml-2 mb-6 text-lg font-bold">Log In</h1>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                        Username
-                    </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="username" id="username" type="text" placeholder="Username"/>
-                </div>
-                <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                        Password
-                    </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="password" id="password" type="password" placeholder="**********"/>
-                    <p className="text-red-500 text-xs italic">{dataAction?.errorMessage}</p>
-                </div>
-                <div className="flex items-center justify-between">
+    console.log(userId);
+    console.log("HERE")
+
+    if (userId) {
+        return (
+            <div className="w-full max-w-xs">
+                <Form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="post" action="/logout">
+                    <h1 className="ml-2 mb-6 text-lg font-bold">You are already Logged in</h1>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white ml-4 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                        Log In
+                            Log Out
                     </button>
-                    <Link className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" to="/sorry">
-                        Forgot Password?
-                    </Link>
-                </div>
-                <div className="flex items-center justify-between">
-                    <Link className=" mt-6 inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" to="/signup">
-                        Create Account
-                    </Link>
-                </div>
-            </Form>
-        </div>
-    )
+                </Form>
+            </div>
+        )
+    } else {    
+        return (
+            <div className="w-full max-w-xs">
+                <Form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="post" reloadDocument>
+                    <h1 className="ml-2 mb-6 text-lg font-bold">Log In</h1>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                            Username
+                        </label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="username" id="username" type="text" placeholder="Username"/>
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                            Password
+                        </label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="password" id="password" type="password" placeholder="**********"/>
+                        <p className="text-red-500 text-xs italic">{dataAction?.errorMessage}</p>
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white ml-4 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                            Log In
+                        </button>
+                        <Link className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" to="/sorry">
+                            Forgot Password?
+                        </Link>
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <Link className=" mt-6 inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" to="/signup">
+                            Create Account
+                        </Link>
+                    </div>
+                </Form>
+            </div>
+        )
+}
 }
