5c02104fb6befcf07313c94898053b17f024656e
Daniel <birkusdaniel6@gmail.com>
2023-04-16 00:36:51 +0200

    I think maybe the delete functionality works now

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,37 +1,49 @@
-import { json } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import { useLoaderData, Link, Form } from "@remix-run/react";
 import connectDb from "~/db/dbConnection.server";
 import { getSession } from "~/session.server";
 
 import { BsHeartFill, BsHeart } from 'react-icons/bs';
+import Modal from "~/components/modal";
 
 export async function action({ request }) {
   const formData = await request.formData();
-  const data = Object.fromEntries(formData);
-  console.log(data);
-  const db = connectDb();
-  const post = await db.models.Post.findById(data.postId);
-  console.log(post)
-  console.log("FROM LIKEPOST FUNCTION")
-  console.log(post.starredBy.includes(data.userId))
-  // check if user is in starredBy
-  if (post.starredBy.includes(data.userId)) {
-      // remove user from starredBy
-      post.starredBy = post.starredBy.filter((id) => id !== data.userId);
-      post.starredByNames = post.starredByNames.filter((name) => name !== data.username);
-      console.log("USER ALREADY LIKED")
-      // update post
-      await post.save();
-  } else {
-      console.log("USER LIKED")
-      // add user to starredBy
-      post.starredBy.push(data.userId);
-      post.starredByNames.push(data.username);
-      // update post
-      await post.save();
+  if (formData.get("action") === "likePost") {
+    const data = Object.fromEntries(formData);
+    console.log(data);
+    const db = connectDb();
+    const post = await db.models.Post.findById(data.postId);
+    console.log(post)
+    console.log("FROM LIKEPOST FUNCTION")
+    console.log(post.starredBy.includes(data.userId))
+    // check if user is in starredBy
+    if (post.starredBy.includes(data.userId)) {
+        // remove user from starredBy
+        post.starredBy = post.starredBy.filter((id) => id !== data.userId);
+        post.starredByNames = post.starredByNames.filter((name) => name !== data.username);
+        console.log("USER ALREADY LIKED")
+        // update post
+        await post.save();
+    } else {
+        console.log("USER LIKED")
+        // add user to starredBy
+        post.starredBy.push(data.userId);
+        post.starredByNames.push(data.username);
+        // update post
+        await post.save();
+    }
+    console.log("Like Post Done")
+    return json({ success: true });
   }
-  console.log("Like Post Done")
-  return json({ success: true });
+  if (formData.get("action") === "deletePost") {
+    const data = Object.fromEntries(formData);
+    console.log(data);
+    const db = connectDb();
+    const deletedPost = await db.models.Post.findByIdAndDelete(data.postId);
+    console.log(deletedPost)
+    console.log("FROM DELETEPOST FUNCTION")
+    return redirect("/");
+}
 }
 
 export async function loader({ request }) {
@@ -61,29 +73,39 @@ export default function Index() {
   console.log(posts)
   console.log("HERE POSTS")
 
+  let showModal = false;
+
     return posts.map(post => (
         <article key={post._id}>
             <div className="max-w-sm rounded overflow-hidden shadow-lg">
                 <div className="px-6 py-4">
                     {/* TODO Delete */}
-                    <div className="font-bold text-xl mb-2">TITLE:{post.title}</div>
-                    <Link to={'/user/' + post.postedBy} className="text-xs- mb-2">USER:<span className='underline'>{post.postedByUser}</span></Link>
-                    <Link to={'/user/' + post.postedBy + '/' + post._id} className="text-xs- mb-2">POST:<span className='underline'>{post.title}</span></Link>
-                    <p className="text-gray-700 text-base">BODY:{post.body}</p>
-                    <p className="text-gray-700 text-base">STARREDBY:{post.starredByNames}</p>
+                    <div className="font-bold text-xl mb-2">{post.title}</div>
+                    <Link to={'/user/' + post.postedBy} className="text-xs- mb-2">Posted by: <span className='underline'>{post.postedByUser}</span></Link>
+                    {/* <Link to={'/user/' + post.postedBy + '/' + post._id} className="text-xs- mb-2">POST:<span className='underline'>{post.title}</span></Link> */}
+                    <p className="text-gray-700 text-base">{post.body}</p>
+                    {/* <p className="text-gray-700 text-base">STARREDBY:{post.starredByNames}</p> */}
                 </div>
                 <div className="px-6 pt-2 pb-4">
-                    <Form className="inline-block px-3 py-1 ml-2 mr-2 mb-2" method='post'>
+                    <Form className="inline-block px-1 py-1 ml-1 mr-1 mb-2" method='post'>
                         <input type="hidden" name='userId' value={user.userId} />
                         <input type="hidden" name='username' value={user.username} />
                         <input type="hidden" name='postId' value={post._id} />
-                        <button>
+                        <button type="submit" name="action" value="likePost">
                             {isStarredBy(post, user) ? <BsHeartFill size={32} className="inline-block mr-2"/> : <BsHeart  size={32} className="inline-block mr-2"/>}
                         </button>
                     </Form>
+                    <Link to={'/user/' + post.postedBy + '/' + post._id}><span className="inline-block bg-gray-200 rounded-full px-3 py-1 ml-2 text-sm font-semibold text-gray-700 mr-2 mb-2">Detail</span></Link>
+                    <button onClick={!showModal} type="button" data-modal-target="crypto-modal" data-modal-toggle="crypto-modal"  className="inline-block bg-gray-200 rounded-full px-3 py-1 ml-2 text-sm font-semibold text-gray-700 mr-2 mb-2">See Likes</button>
+                    {showModal && <Modal>}
+                            
 
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 ml-2 text-sm font-semibold text-gray-700 mr-2 mb-2">See All Likes</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 ml-2 text-sm font-semibold text-gray-700 mr-2 mb-2">Delete Post</span>
+                    <Form  className="inline-block bg-gray-200 rounded-full px-3 py-1 ml-2 text-sm font-semibold text-gray-700 mr-2 mb-2"  method="post">
+                      <input type="hidden" id="postId" name="postId" value={post._id}/>
+                      <button type="submit" name="action" value="deletePost">
+                        Delete
+                      </button>
+                    </Form>
                 </div>
             </div>
         </article>
