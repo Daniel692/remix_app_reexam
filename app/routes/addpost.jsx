a4d746ff9c9f150af2cd4db7d01cbab62c89b1b8
Daniel <birkusdaniel6@gmail.com>
2023-04-16 18:28:09 +0200

    addpost limited to 140 cahrs

--- app/routes/addpost.jsx
+++ app/routes/addpost.jsx
@@ -4,12 +4,22 @@ import connectDb from "~/db/dbConnection.server";
 import { Form, useActionData, useLoaderData } from '@remix-run/react';
 // import { json } from '@remix-run/node';
 import { getSession } from '~/session.server';
+import { useState } from "react";
 
 export default function AddPostPage() {
-        const data = useActionData()
+    const data = useActionData()
+    const  user  = useLoaderData();
 
-        const  user  = useLoaderData();
-        console.log(user);
+    const [ post, setPost ] = useState("")
+
+    function handlePostChange(e) {
+        const inputPost = e.target.value
+        if (inputPost.length <= 140) {
+            setPost(inputPost)
+        }
+    }
+    
+
     return <main>
         <div className='w-full max-w-xs'>
             <Form method="post" id="post-form" className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -17,11 +27,12 @@ export default function AddPostPage() {
                     <input type="hidden" id="postedByUser" name="postedByUser" value={user.username} />
                 <div className="mb-6">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="title">Title:</label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" id="title" name="title" placeholder='Title' required />
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" id="title" name="title" placeholder='Title' required maxLength={50}/>
                 </div>
                 <div className="mb-6">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="body">body:</label>
-                    <textarea className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="body" name="body" rows="5" placeholder='Body Text' required />
+                    <textarea className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="body" name="body" rows="6" cols="50" placeholder='Body Text' value={post} required maxLength="140" onChange={handlePostChange}/>
+                    <p>{140 - post.length} characters remaining</p>
                 </div>
                 <div className="flex items-center justify-between">
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type='submit'>Add Post</button>
@@ -37,7 +48,7 @@ export async function loader({ request }) {
     if (!session.get("userId")) {
         return redirect("/login");
       }
-      
+
     return json({ userId: session.get("userId"), username: session.get("username") });
   }
 
