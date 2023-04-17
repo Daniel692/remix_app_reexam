cf065c80c956b2ddfe178930a3ff7c203229b35f
Daniel <birkusdaniel6@gmail.com>
2023-04-17 15:09:10 +0200

    CSS added

--- app/routes/addpost.jsx
+++ app/routes/addpost.jsx
@@ -21,25 +21,24 @@ export default function AddPostPage() {
     
 
     return <main>
-        <div className='w-full max-w-xs'>
-            <Form method="post" id="post-form" className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                    <input type="hidden" id="postedBy" name="postedBy" value={user.userId} />
-                    <input type="hidden" id="postedByUser" name="postedByUser" value={user.username} />
-                <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="title">Title:</label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" id="title" name="title" placeholder='Title' required maxLength={50}/>
-                </div>
-                <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="body">body:</label>
-                    <textarea className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="body" name="body" rows="6" cols="50" placeholder='Body Text' value={post} required maxLength="140" onChange={handlePostChange}/>
-                    <p className="text-gray-700 text-xs italic">{140 - post.length} characters remaining</p>
-                </div>
-                <div className="flex items-center justify-between">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type='submit'>Add Post</button>
-                </div>
-            </Form>
+        <div className="w-full max-w-md mx-auto my-5">
+        <Form method="post" id="post-form" className="bg-gray-100 shadow-md rounded px-8 pt-6 pb-8">
+            <input type="hidden" id="postedBy" name="postedBy" value={user.userId} />
+            <input type="hidden" id="postedByUser" name="postedByUser" value={user.username} />
+            <div className="mb-6">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="title">Title:</label>
+            <input className="appearance-none bg-gray-200 border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" id="title" name="title" placeholder="Enter a title" required maxLength={50} />
+            </div>
+            <div className="mb-6">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="body">Body:</label>
+            <textarea className="appearance-none bg-gray-200 border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="body" name="body" rows="6" cols="50" placeholder="Enter some text" value={post} required maxLength="140" onChange={handlePostChange} />
+            <p className="text-gray-700 text-xs italic text-right">{140 - post.length} characters remaining</p>
+            </div>
+            <div className="flex items-center justify-end">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Add Post</button>
+            </div>
+        </Form>
         </div>
-
     </main>
 }
 
